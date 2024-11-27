47209f3b1423d9e5d449cf2daddb8b69483f8c98
Simran Bhandari <simranbhandari036@gmail.com>
2024-11-27 17:37:01 +0530

    Log Collections Pipeline

--- build-server/script.js
+++ build-server/script.js
@@ -3,7 +3,9 @@ const path = require('path')
 const fs = require('fs')
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 const mime = require('mime-types')
+const Redis = require('ioredis')
 
+const publisher = new Redis('')
 
 const s3Client = new S3Client({
     region: '',
@@ -15,30 +17,40 @@ const s3Client = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID
 
+function publishLog(log) {
+    publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
+}
+
 async function init() {
     console.log('Executing script.js')
+    publishLog('Build Started...')
     const outDirPath = path.join(__dirname, 'output')
 
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
 
     p.stdout.on('data', function (data) {
         console.log(data.toString())
+        publishLog(data.toString())
     })
 
     p.stdout.on('error', function (data) {
         console.log('Error', data.toString())
+        publishLog(`error: ${data.toString()}`)
     })
 
     p.on('close', async function () {
         console.log('Build Complete')
+        publishLog(`Build Complete`)
         const distFolderPath = path.join(__dirname, 'output', 'dist')
         const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
 
+        publishLog(`Starting to upload`)
         for (const file of distFolderContents) {
             const filePath = path.join(distFolderPath, file)
             if (fs.lstatSync(filePath).isDirectory()) continue;
 
             console.log('uploading', filePath)
+            publishLog(`uploading ${file}`)
 
             const command = new PutObjectCommand({
                 Bucket: 'vercel-clone-outputs',
@@ -48,8 +60,10 @@ async function init() {
             })
 
             await s3Client.send(command)
+            publishLog(`uploaded ${file}`)
             console.log('uploaded', filePath)
         }
+        publishLog(`Done`)
         console.log('Done...')
     })
 }
